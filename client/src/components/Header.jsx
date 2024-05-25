e00942f1dc44622bce12faa81a4fdbc1c2f6ba3b
gaurav <gauravkumarkarki07@gmail.com>
2024-05-25 21:13:37 +1000

    Header changed background

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,23 +4,23 @@ import {Link} from 'react-router-dom';
 export default function Header() {
   return (
     <header>
-        <nav className="flex justify-between px-14 items-center py-2 bg-primaryThird font-semibold text-white">
+        <nav className="flex justify-between px-14 items-center py-2 bg-cyan-800 font-semibold text-white">
             <div className='text-2xl'>
-                <Link to='/' className='hover:text-cyan-800'>
+                <Link to='/' className='hover:text-cyan-950'>
                     <span>find</span>
-                    <span className='text-cyan-800'>Home</span>
+                    <span className='text-cyan-950'>Home</span>
                 </Link>
             </div>
             <ul className='flex gap-5 items-center'>
-                <Link to='/' className='hover:bg-cyan-800 px-2 py-2 rounded-lg'>
+                <Link to='/' className='hover:bg-cyan-950 px-2 py-2 rounded-lg'>
                     <li>Home</li>
                 </Link>
-                <Link to='/aboutus'className='hover:bg-cyan-800 px-2 py-2 rounded-lg' >
+                <Link to='/aboutus'className='hover:bg-cyan-950 px-2 py-2 rounded-lg' >
                     <li>AboutUs</li>
                 </Link>
             </ul>
             <div>
-                <Link to='/login' className='hover:bg-cyan-800 px-2 py-2 rounded-lg'>
+                <Link to='/login' className='hover:bg-cyan-950 px-2 py-2 rounded-lg'>
                     <span>Login</span>
                 </Link>
             </div>
--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,6 +1,35 @@
-
+import AboutUsPage from '../assets/about-us.png';
 export default function AboutUs() {
   return (
-    <div>AboutUs</div>
+    <div>
+      <section className="px-24 bg-gradient-to-r from-cyan-600 via-background to-primary">
+        <div className="flex gap-6 justify-center items-center">
+          <div className='flex flex-col gap-8 px-6'>
+          <h1 className='font-semibold text-4xl'>About Us</h1>
+
+          <p className='text-white text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Dignissimos aut tempora repudiandae aperiam cupiditate cumque provident molestias ducimus, 
+            labore quidem inventore rerum tenetur fugiat quasi voluptatibus a veniam. Iure, expedita.
+          </p>
+          </div>
+          <img src={AboutUsPage}/>
+        </div>
+      </section>
+      <section className='flex gap-6 justify-center items-center px-14 py-8 text-white h-72'>
+        <div className='flex flex-col gap-3 border px-4 py-4 bg-cyan-500 rounded-lg h-full'>
+          <h2 className='font-semibold text-2xl text-black'>Our Vision</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit mollitia eligendi doloremque voluptates 
+            ex similique incidunt nemo eos voluptate. Corporis dolor ea nam et nulla, amet maiores hic nihil nesciunt.
+          </p>
+        </div>
+        <div className='flex flex-col gap-3 border px-4 py-4 bg-cyan-500 rounded-lg h-full'>
+          <h2 className='font-semibold text-2xl text-black'>Our Values</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ex exercitationem non nam? 
+            Iusto modi quo similique! Cumque quidem, vel, nesciunt, unde fuga blanditiis explicabo eius fugit magni provident totam?
+          </p>
+        </div>
+      </section>
+    </div>
   )
 }
